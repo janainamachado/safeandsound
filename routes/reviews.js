d57d15ddddb37b351eda1a2e27bfe828274cf394
Janaina Machado <janainamachado@Janainas-MacBook-Pro.local>
2024-10-23 15:38:47 -0300

    prevent sql injection

--- routes/reviews.js
+++ routes/reviews.js
@@ -18,22 +18,23 @@ router.post("/", async function(req, res, next) {
     let geoloc = await geocode(address);
     let lat = geoloc.data.latLng[0];
     let longitude = geoloc.data.latLng[1];
-    let sql = `INSERT INTO venues (address, venuename, lat, longitude)
-        VALUES ("${address}", "${venuename}", "${lat}", "${longitude}")`;
-    await db(sql);
-    sql = `SELECT * FROM venues WHERE venuename = "${venuename}"`;
-    let response = await db(sql);
+
+    let sql = `INSERT INTO venues (address, venuename, lat, longitude) VALUES (?, ?, ?, ?)`;
+    await db(sql, [address, venuename, lat, longitude]);
+
+    sql = `SELECT * FROM venues WHERE venuename = ?`;
+    let response = await db(sql, [venuename]);
     let data = response.data;
     venue_id = data[0].id;
   }
+
   let { date, incident, username } = req.body;
-  sql = `INSERT INTO reviews (venue_id, date, incident, username)
-    VALUES ("${venue_id}","${date}", "${incident}", "${username}")
-    `;
-  await db(sql);
+  sql = `INSERT INTO reviews (venue_id, date, incident, username) VALUES (?, ?, ?, ?)`;
+  await db(sql, [venue_id, date, incident, username]);
+
   let lastInsertedReviewSQL = `SELECT DATE_FORMAT(r.date, '%Y-%m-%d'), r.incident, r.username, r.venue_id, v.venuename, v.address
     FROM reviews r
-    INNER JOIN venues v ON r.venue_id=v.id ORDER BY r.id DESC`;
+    INNER JOIN venues v ON r.venue_id = v.id ORDER BY r.id DESC`;
   let result = await db(lastInsertedReviewSQL);
   let review = result.data[0];
   res.status(201).send(review);
